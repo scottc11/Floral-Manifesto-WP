d6f75ddc66a9282364f8735686e00654f2263cd5
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-08-19 13:02:42 -0400

    removed default copyright content in footer.

--- wp-content/themes/html5blank/footer.php
+++ wp-content/themes/html5blank/footer.php
@@ -30,12 +30,7 @@
 
 
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a>.
-				</p>
-				<!-- /copyright -->
+				
 
 			</footer>
 			<!-- /footer -->
