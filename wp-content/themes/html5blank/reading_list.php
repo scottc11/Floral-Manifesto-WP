cdc1e98a256dd90371489958e58149133083940b
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-09-06 09:37:01 -0400

    added all the reading list custom posts to database, including images of books.  Built a simple function which seperates readings into rows of three using html markup.

--- wp-content/themes/html5blank/reading_list.php
+++ wp-content/themes/html5blank/reading_list.php
@@ -28,145 +28,76 @@ Template Name: Reading List
       </div>
 
 
-			<!-- START OF LOOP -->
-			<?php
-
-				// define the post type for the loop
-				$args = array(
-					'post_type' => 'fm_reading',
-					'supports' => array('title','editor','custom-fields')
-				);
-
-				// Create a new WP loop query and hand it the custom post type arguments
-				$loop = new WP_Query( $args );
-
-				if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post();
-
-			?>
-
+			<!-- 		 BOOKS  		-->
 			<section class="row">
 
-				<div class="row">
-					
-				</div>
+				<!-- START OF LOOP -->
+				<?php
+
+					// define the post type for the loop
+					$args = array(
+						'post_type' => 'fm_reading',
+						'supports' => array('title','editor','custom-fields')
+					);
+
+					// Create a new WP loop query and hand it the custom post type arguments
+					$loop = new WP_Query( $args );
+
+					// for inserting a row for every 3rd book/reading post
+					$counter = 0;
+
+					if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post();
+
+
+
+				?>
+
+
+				<!-- STARTING DIV ROW FOR 1ST BOOK IN ROW -->
+				<?php if ($counter == 0) { ?>
+					<div class="row">
+				<?php } ?>
+
+
+					<div class="book-container col-xs-12 col-sm-4">
+
+						<!-- Post thumbnail -->
+						<div class="book-image-container">
+
+							<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+								<?php the_post_thumbnail(); ?>
+							<?php endif; ?>
+
+						</div>
+						<!-- /post thumbnail -->
+
+						<!-- Book Title -->
+						<h2><?php the_title(); ?></h2>
+						<!-- /Book Title -->
+
+						<p>
+							<?php the_content(); ?>
+						</p>
+
+					</div>
+
+				<?php $counter++ ?>
+				<!-- ENDING DIV ROW FOR 3RD BOOK IN ROW -->
+				<?php if ($counter == 3) { ?>
+						</div>
+				<?php
+						$counter = 0;
+			 		}
+				?>
+
+
+
+				<!-- END OF LOOP -->
+				<?php endwhile; else: ?>
+					<p><?php _e( 'The Reading you are looking for could not be found.' ); ?></p>
+				<?php endif; ?>
 
 			</section>
-
-
-			<!-- END OF LOOP -->
-			<?php endwhile; else: ?>
-				<p><?php _e( 'The Reading you are looking for could not be found.' ); ?></p>
-			<?php endif; ?>
-
-
-      <!-- THE BOOKS -->
-      <div class="row">
-
-
-        <div class="row">
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/the-argonauts.png" alt="" />
-            </div>
-            <h2>The Argonauts, Maggie Nelson</h2>
-            <p>
-              The Argonauts is everything that I want to be as a writer. Seen as one of the pioneering texts in the genre known as “auto-theory,” Nelson artfully blends memoir and critical theory in order to tell the story of becoming a mother at the same time as her partner is transitioning. Nelson’s text also grapples with the ethics of writing, of representation, and radical intimacy. After I finished reading The Argonauts, I laid down and had a gentle cry.
-            </p>
-          </div>
-
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/wuthering-heights.png" alt="" />
-            </div>
-            <h2>Wuthering Heights, Emily Brontë</h2>
-            <p>
-              You’ve probably heard the Kate Bush song, in which Cathy sings to the man she loves but can never be with – because, well, class and race and all that stuff that people really cared about, especially in the 19th century. But if you haven’t read the book, you should get on that ASAP. This is one of the books that made me fall in love with literature and helped me understand that love, is, well, complicated. As Kate Bush/Cathy sings: “I hated you. I loved you, too.”
-            </p>
-          </div>
-
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/collected-stories-of-carson-mccullers.png" alt="" />
-            </div>
-            <h2>Collected Stories of Carson McCullers, Carson McCullers</h2>
-            <p>
-              Despite the fact that Tennessee Williams proclaimed McCullers to be the most important writer of the 20th century, very few people have ever heard of her. McCullers is most famous for The Heart is a Lonely Hunter (1946), published when she was just 22! Her novella, The Member of the Wedding, is perhaps my favourite. Set in the American south, the story follows 12-year old Frankie as she tries to find her “we of me.” McCullers’ stories are populated by freaks, strange girls, and misfits who’re all trying to figure out how to foster intimate relationships with others.
-            </p>
-          </div>
-        </div>
-
-
-        <div class="row">
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/what-purpose-did-i-serve-in-your-life.png" alt="" />
-            </div>
-            <h2>what purpose did i serve in your life, Marie Calloway</h2>
-            <p>
-              I might be one of the only people ever to say, “I love this book.” Calloway’s text blurs the lines between fiction and memoir, as it offers 12 stories in which the protagonist, also named Marie, follows her desires. From losing her virginity to a man she just met to fleeting moments of prostitution, her sexual odyssey addresses the ambivalent nature of desire: sometimes the things Marie wants don’t make her feel so good. Instead of judging herself, Marie recounts these experiences with a detachment that unsettles readers. But that’s the whole point.
-            </p>
-          </div>
-
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/citizen-an-american-lyric.png" alt="" />
-            </div>
-            <h2>Citizen: An American Lyric, Claudia Rankine</h2>
-            <p>
-              I can’t count the number of times that I picked up this book, read a section, and had to put it down. As a white woman living in Canada, Citizen made me confront my white privilege in a way that was deeply uncomfortable and painful – just as it should be. Rankine’s collection of poetry, which is also in part a memoir, is not just about addressing the rampant systemic racism in America, but about anger and grief and the ways in which those feelings are deemed inappropriate by the white status quo. Rankine’s text is necessary reading.
-            </p>
-          </div>
-
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/rookie-yearbook.png" alt="" />
-            </div>
-            <h2>Rookie Yearbook</h2>
-            <p>
-              Words can’t begin to describe how much I love Rookie Magazine, an online magazine for teen girls started by Tavi Gevinson in 2011. Rookie is everything that I wish I’d had when I was a teen girl: stories that address – but refuse to judge – all of the feelings that teen girls have. With stories like “Everybody Farts” and “How to Make it Look Like You Weren’t Just Crying in Less Than Five Minutes,” Rookie is not just a breath of fresh air, but it’s vital reading for teens and adults alike.
-            </p>
-          </div>
-        </div>
-
-
-        <div class="row">
-
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/the-empathy-exams.png" alt="" />
-            </div>
-            <h2>The Empathy Exams, Leslie Jamison</h2>
-            <p>
-              I can’t count the number of times I’ve reference Jamison’s essay “The Grand Unified Theory of Female Pain.” Like Maggie Nelson, Jamison is a major inspiration for my own writing. Her ability to blend theory and her personal experiences appears effortless. If you want to think about the ways in which we talk about bodies and the ways in which we subject ourselves to pain, seek it out, the pains that we’ve experienced against our will and need to put into language, and the how to foster empathy (towards yourself and others) you should read this book.
-            </p>
-          </div>
-
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/diary-of-a-teenage-girl.png" alt="" />
-            </div>
-            <h2>Diary of a Teenage Girl, Phoebe Gloeckner</h2>
-            <p>
-              Reading Gloeckner’s Diary was a revelation. Most stories of female coming of age that involve losing one’s virginity end with death, abandonment, disease, and I every other bad thing ever. As someone who lost her virginity at a young age, who enjoyed sex but didn’t know how to seek out healthy sexual relations, and who managed to figure things out in the end, I’ve always wanted to read a text that reflected my experience – and what I imagine to be the experiences of many young girls. Gloeckner’s graphic novel did just that.
-            </p>
-          </div>
-
-          <div class="book-container col-xs-12 col-sm-4">
-            <div class="book-image-container">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/readinglist/h-is-for-hawk.png" alt="" />
-            </div>
-            <h2>H is For Hawk, Helen MacDonald</h2>
-            <p>
-              Grief is perhaps one of the hardest emotions we’ll ever have to deal with. In MacDonald’s memoir she recounts how she undertook the task of training a goshawk, and how this task forced her to confront the loss of her father.
-            </p>
-            <p>
-              As someone who lost her mother at a young age, I’ve always been drawn to narratives that talk about loss and the ways in which that loss shapes the lives of those left behind. More recently, I’ve begun to think more about the ways in which we process – or fail to process – grief.
-            </p>
-          </div>
-        </div>
-
-      </div>
-
 
     </div>
   </main>
